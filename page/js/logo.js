385b215bfca18879070528ee83c51de846f58db8
Timur Sattarov <tim.helloworld@gmail.com>
2012-12-29 03:41:45 +0400

    making logo scaled

--- page/js/logo.js
+++ page/js/logo.js
@@ -1,8 +1,22 @@
+function calcDistance(x1,y1,x2,y2) {
+    var dx = x2 - x1;
+    var dy = y2 - y1;
+    return Math.sqrt(dx * dx + dy * dy);
+}
+
+function calcLenght(dx,dy) {
+    var dx = x2 - x1;
+    var dy = y2 - y1;
+    return Math.sqrt(dx * dx + dy * dy);
+}
+
 function Vector(x,y) {
     this.x = x;
     this.y = y;
     this.len = function() {
-        return Math.sqrt(this.x * this.x + this.y * this.y);
+        var x = this.x;
+        var y = this.y;
+        return Math.sqrt(x*x+y*y);
     };
     
     this.cosA = function(v) {
@@ -20,11 +34,11 @@ function Rectangle(x1,y1,x2,y2) {
     this.getDistance = function(x, y) {
         if ( x < this.x1 ) {
             if ( y < this.y1) {
-                return new Vector(x - this.x1, y - this.y1).len();
+                return calcDistance(x, y, this.x1, this.y1);
             } else if ( y < this.y2) {
                 return this.x1 - x;
             } else {
-                return new Vector(x - this.x1, y - this.y2).len();
+                return calcDistance(x, y, this.x1, this.y2);
             }
         } else if ( x < this.x2) {
             if ( y < this.y1) {
@@ -36,11 +50,11 @@ function Rectangle(x1,y1,x2,y2) {
             }
         } else {
             if ( y < this.y1) {
-                return new Vector(x - this.x2, y - this.y1).len();
+                return calcDistance(x, y, this.x2, this.y1);
             } else if ( y < this.y2) {
                 return x - this.x2;
             } else {
-                return new Vector(x - this.x2, y - this.y2).len();
+                return calcDistance(x, y, this.x2, this.y2);
             }
         }
     } 
@@ -50,17 +64,19 @@ var c;
 var ctx;
 var imgData;
 
-var i = 0;
+var gTime = 0;
 
-var width=100;
-var height=100;
-var halfWidth=Math.round(width/2);
-var halfHeight=Math.round(height/2);
-var mouseCoords=new Vector(0,0);
+var gWidth=64;
+var gHeight=64;
+var scaleFactor=300/800;
+var gHalfWidth=Math.round(gWidth/2);
+var gHalfHeight=Math.round(gHeight/2);
+var mouseCoords=new Vector(-100,-100);
 
-var backRect = new Rectangle(25, 25 , 75, 75);
+var backRect = new Rectangle(20, 20 , 44, 44);
 
 function P(sA, isAddPi) {
+    var i = gTime;
     var p =(Math.acos(sA)*6);
     if (isAddPi)
         return Math.cos( i * 0.2 - p );
@@ -69,6 +85,12 @@ function P(sA, isAddPi) {
 }
 
 function animation() {
+    var iData = imgData.data;
+    var height = gHeight;
+    var width = gWidth;
+    var halfWidth = gHalfWidth;
+    var halfHeight = gHalfHeight;
+    var i = gTime;
     for (var y = 0; y < height; y++) {
         for (var x = 0; x < width; x++) {
             vi = new Vector( x-halfWidth, y-halfHeight);
@@ -76,11 +98,11 @@ function animation() {
             p = (y * width + x) * 4;
             mp = new Vector( mouseCoords.x - x, mouseCoords.y - y);
             md = mp.len() > 50 ? 0 : 50 - mp.len();
-            s = backRect.getDistance(x,y)*10.9 + P(v0.cosA(vi), y > halfHeight) * 10 + md*2;
-            imgData.data[p + 0] = (Math.cos(0.017 * (y + i)) + 1) * 127;
-            imgData.data[p + 1] = (Math.sin(0.02 * (i - y + x)) + Math.sin(0.005 * (mp.len())) + 2) * 64;
-            imgData.data[p + 2] = (Math.cos(0.045 * (y + i)) + 1) * 127;
-            imgData.data[p + 3] = s < 255 ? 255 - s : 0;
+            s = backRect.getDistance(x,y)*12.9 + P(v0.cosA(vi), y > halfHeight) * 10 + md*2;
+            iData[p + 0] = (Math.cos(0.017 * (y + i)) + 1) * 127;
+            iData[p + 1] = (Math.sin(0.02 * (i - y + x)) + 1) * 127;
+            iData[p + 2] = (Math.cos(0.045 * (y + i)) + 1) * 127;
+            iData[p + 3] = s < 255 ? 255 - s : 0;
         }
     }
     
@@ -89,15 +111,15 @@ function animation() {
 //    ctx.drawImage(imgData, 0, 0);
 }
 
+function onMouseMove(e) {
+    mouseCoords.x = (e.pageX - c.parentNode.offsetLeft)*scaleFactor;
+    mouseCoords.y = (e.pageY - c.parentNode.offsetTop)*scaleFactor;
+}
+
 function initLogo() {
     c = document.getElementById("box1");
     ctx = c.getContext("2d");
-//    ctx.scale(2,2);
-    imgData = ctx.createImageData(width,height);
-    document.addEventListener('mousemove', function (e) {
-        mouseCoords.x = e.pageX - c.offsetLeft;
-        mouseCoords.y = e.pageY - c.offsetTop;
-
-    }, 0);
+    imgData = ctx.createImageData(gWidth,gHeight);
+    document.addEventListener('mousemove', onMouseMove, 0);
 
 }
